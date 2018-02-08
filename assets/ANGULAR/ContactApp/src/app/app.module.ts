0abaa96bb0e41a1f0691b892c1928f1ee51db569
Andrei-Rosu <jamaika80@yahoo.com>
2018-02-08 17:30:14 +0100

    Travail du 08/02/2018p
    Commencement de travail sur l'ap

---
+++ assets/ANGULAR/ContactApp/src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+
+
+import { AppComponent } from './app.component';
+import {FormsModule} from '@angular/forms';
+import { ProfilComponent } from './profil/profil.component';
+import { AjouterComponent } from './ajouter/ajouter.component';
+import {HttpClientModule} from '@angular/common/http';
+import {UserApiService} from './shared/services/user-api.service';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ProfilComponent,
+    AjouterComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpClientModule
+  ],
+  providers: [
+    UserApiService
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
