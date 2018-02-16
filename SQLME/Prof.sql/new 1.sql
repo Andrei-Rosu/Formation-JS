5c46fbbf9ed99ab924ff539748110c48b27e7ca0
Andrei-Rosu <jamaika80@yahoo.com>
2018-02-16 17:24:55 +0100

    mySQL IV

--- SQLME/Prof.sql/new 1.sql
+++ SQLME/Prof.sql/new 1.sql
@@ -9,37 +9,44 @@ WHERE avc.id_vehicule = 503
 AND c.id_conducteur = avc.id_conducteur;
 
 -- qui conduit quoi
+
+SELECT prenom.c, nom.c, marque.v, modele.v
+FROM conducteur c , association_vehicule_conducteur avc, vehicule v
+WHERE c.id_conducteur = avc.id_conducteur
+AND avc.id_vehicule = v.id_vehicule;
+
+
+
 -- ajoutez vous dans la table conducteur
 	-- afficher tous les conducteurs (sans exception) pour ensuite rajouter les vehicules conduit si c'est le cas
+
+SELECT prenom.c, nom.c, marque.v, modele.v
+FROM conducteur c 
+LEFT JOIN association_vehicule_conducteur avc ON c.id_conducteur = avc.id_conducteur
+LEFT JOIN vehicule c ON avec.id_vehicule= v.id_vehicule;
 
 -- ajoutez un nouveau vehicule
 	-- afficher tous les vehicules (sans exception) pour ensuite rajouter les conducteurs si c'est le cas
 
+SELECT prenom.c, nom.c, marque.v, modele.v
+FROM conducteur c 
+RIGHT JOIN association_vehicule_conducteur avc ON c.id_conducteur = avc.id_conducteur
+RIGHT JOIN vehicule c ON avec.id_vehicule= v.id_vehicule;
+
+
+
+
 -- afficher tous les conducteurs ainsi que tous les vehicules  (sans exception) peu importe les correspondances
 
--- qui conduit la voiture 503
+SELECT prenom.c, nom.c, marque.v, modele.v
+FROM conducteur c 
+LEFT JOIN association_vehicule_conducteur avc ON c.id_conducteur = avc.id_conducteur
+LEFT JOIN vehicule c ON avec.id_vehicule= v.id_vehicule;
+UNION
+SELECT prenom.c, nom.c, marque.v, modele.v
+FROM conducteur c 
+RIGHT JOIN association_vehicule_conducteur avc ON c.id_conducteur = avc.id_conducteur
+RIGHT JOIN vehicule c ON avec.id_vehicule= v.id_vehicule;
 
 
 
--- SELECT id_livre FROM emprunt WHERE id_abonne IN 
-	-- (SELECT id_abonne FROM abonne WHERE prenom = 'chloe');
-
-
--- qui conduit quoi
-SELECT prenom, marque
-FROM conducteur, vehicule
-WHERE conducteur.id_conducteur = vehicule.id_conducteur
-AND vehicule.id_vehicule = association_vehicule_conducteur.id_vehicule;
-
-
-SELECT prenom, nom, marque, modele
-FROM conducteur, vehicule
-WHERE conducteur.id_conducteur = vehicule.id_vehicule
-AND 
-
-
-
-	SELECT prenom, titre, date_sortie
-FROM abonne, livre, emprunt
-WHERE abonne.id_abonne = emprunt.id_abonne
-AND emprunt.id_livre = livre.id_livre; 
